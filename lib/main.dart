7213df98e47b01f108532d2c24cd39cae4126751
ffa13516-cyber <ffa13516@gmail.com>
2026-04-29 12:39:45 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/chat/chat_screen.dart';
 import 'screens/home/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDMbgMfJkuiEc4EmN8S_nOLVIghAZSzQiE",
+      appId: "1:560030093300:android:ad0677ea0cd7b0b36433a1",
+      messagingSenderId: "560030093300",
+      projectId: "messengerapp-d6e7c",
+      databaseURL: "https://messengerapp-d6e7c-default-rtdb.firebaseio.com",
+      storageBucket: "messengerapp-d6e7c.firebasestorage.app",
+    ),
+  );
+
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  /// 🔥 خليناه false عشان يفتح الهوم
-  static const bool openChatDirectly = false;
+  static const bool openChatDirectly = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Chat App',
+      title: 'ChatX',
       theme: ThemeData.dark(),
       home: openChatDirectly
           ? const ChatScreen()
