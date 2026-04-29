bc11f7e09e006cbb81c24cc38e140d5a3a054079
ffa13516-cyber <ffa13516@gmail.com>
2026-04-29 12:52:02 +0300

    Refactor main.dart to simplify app initialization
    Removed Firebase initialization and system UI overlay settings. Changed app title and modified behavior to open home screen by default.

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'screens/chat/chat_screen.dart';
 import 'screens/home/home_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyDMbgMfJkuiEc4EmN8S_nOLVIghAZSzQiE",
-      appId: "1:560030093300:android:ad0677ea0cd7b0b36433a1",
-      messagingSenderId: "560030093300",
-      projectId: "messengerapp-d6e7c",
-      databaseURL: "https://messengerapp-d6e7c-default-rtdb.firebaseio.com",
-      storageBucket: "messengerapp-d6e7c.firebasestorage.app",
-    ),
-  );
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-    ),
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  static const bool openChatDirectly = true;
+  /// 🔥 خليناه false عشان يفتح الهوم
+  static const bool openChatDirectly = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ChatX',
+      title: 'Chat App',
       theme: ThemeData.dark(),
       home: openChatDirectly
           ? const ChatScreen()
