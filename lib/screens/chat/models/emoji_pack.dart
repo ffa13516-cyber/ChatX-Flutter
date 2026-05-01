eca6b643f3aeaeb54433cc360941c0c831e1958d
ffa13516-cyber <ffa13516@gmail.com>
2026-05-01 04:29:55 +0300

    Add EmojiPack class for managing emoji collections

---
+++ lib/screens/chat/models/emoji_pack.dart
@@ -0,0 +1,11 @@
+import 'emoji_model.dart';
+
+class EmojiPack {
+  final String name;
+  final List<EmojiModel> emojis;
+
+  EmojiPack({
+    required this.name,
+    required this.emojis,
+  });
+}
