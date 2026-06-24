e478146fac220c3fe04b0be146379d927988aaa1
ffa13516-cyber <ffa13516@gmail.com>
2026-06-25 00:29:40 +0300

    Add myUid and myName parameters to HomeScreen

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -5,25 +5,31 @@ import 'home_cubit.dart';
 import 'home_screen_ui.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final String myUid;
+  final String myName;
+
+  const HomeScreen({
+    super.key,
+    required this.myUid,
+    required this.myName,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // بنوفر الـ Cubit للشاشة دي
     return BlocProvider(
       create: (context) => HomeCubit(),
       child: BlocBuilder<HomeCubit, HomeState>(
         builder: (context, state) {
-          // بنجيب نسخة من الـ Cubit عشان نستخدم الـ Functions بتاعته
           final cubit = context.read<HomeCubit>();
 
-          // بنرجع الـ UI اللي فصلناه، وبنبعتله الداتا واللوجيك
           return HomeScreenUI(
             currentIndex: state.currentIndex,
             onTabSelected: cubit.changeTab,
             onCreateChannel: cubit.createChannel,
             onCreateGroup: cubit.createGroup,
             onSearch: cubit.search,
+            myUid: myUid,
+            myName: myName,
           );
         },
       ),
